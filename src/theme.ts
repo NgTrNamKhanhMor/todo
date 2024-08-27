3b02b9e8ccfa50d6c522e1b4f9fe0a1bb5e21487
NgTrNamKhanh <khanh.ntn@mor.com.vn>
2024-08-27 10:00:27 +0700

    project init

---
+++ src/theme.ts
@@ -0,0 +1,42 @@
+
+import { extendTheme } from '@mui/joy/styles';
+
+
+
+const theme = extendTheme({
+  "colorSchemes": {
+    "light": {
+      "palette": {
+        "primary": {
+          "50": "#fffbeb",
+          "100": "#fef3c7",
+          "200": "#fde68a",
+          "300": "#fcd34d",
+          "400": "#fbbf24",
+          "500": "#f59e0b",
+          "600": "#d97706",
+          "700": "#b45309",
+          "800": "#92400e",
+          "900": "#78350f"
+        },
+        "warning": {
+          "50": "#fffbeb",
+          "100": "#fef3c7",
+          "200": "#fde68a",
+          "300": "#fcd34d",
+          "400": "#fbbf24",
+          "500": "#f59e0b",
+          "600": "#d97706",
+          "700": "#b45309",
+          "800": "#92400e",
+          "900": "#78350f"
+        }
+      }
+    },
+    "dark": {
+      "palette": {}
+    }
+  }
+})
+
+export default theme;
