eec58da0c5a75170271b7a9a8a8702c9e1db4bd9
NgTrNamKhanh <khanh.ntn@mor.com.vn>
2024-09-04 10:01:13 +0700

    Update: change the password validation

--- src/helpers/authValidation.ts
+++ src/helpers/authValidation.ts
@@ -3,7 +3,14 @@ import * as Yup from "yup";
 export const validationSchema = Yup.object({
   email: Yup.string().email("Invalid email address").required("Required"),
   password: Yup.string()
-    .min(6, "Password must be at least 6 characters")
+    .min(8, "Password must be at least 8 characters")
+    .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+    .matches(/\d/, "Password must contain at least one number")
+    .matches(
+      /[@$!%*?&#]/,
+      "Password must contain at least one special character"
+    )
     .required("Required"),
 });
 
